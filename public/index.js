95347615b7a50ebea53de672c1543210953ca776
mat <mathiaslsn2@gmail.com>
2020-07-18 01:36:08 -0400

    Add ooblet changing

--- public/index.js
+++ public/index.js
@@ -33,6 +33,7 @@ function handleTocJSON(e) {
 
     containerSwitch('.step2')
   } catch (err) {
+    console.log(err)
     alert('Unable to parse save file! Ensure you have selected the ooblets_toc.sav file.')
   }
 }
@@ -61,6 +62,7 @@ function handleSavJSON(e) {
     const json = JSON.parse(e.target.result)
     doSaveCollection(json)
   } catch (err) {
+    console.log(err)
     alert('Unable to parse save file!')
   }
 }
@@ -73,12 +75,12 @@ function doSaveCollection(json) {
   const gameManager = window.saveFile.listOfGameManager[1]
   const invManager = json.listOfInventoryManager.filter(inv => inv.playerName === window.playerName)[0]
   const metaManager = window.saveFile.listOfMetaManager[1]
+  const oobletStats = window.saveFile.listOfOobletStats
 
   $('#gummies').val(invManager.money)
   $('#wishies').val(invManager.sparklePoints.amt)
   $('#level').val(invManager.playerLevel)
   $('#wateringCanLevel').val(invManager.wateringCanLevel)
-  $('#name').val(invManager.playerName)
 
   $('#weather').val(gameManager.todayDayTag)
   $('#timeOfDayInTicks').val(gameManager.timeOfDayInTicks)
@@ -87,6 +89,36 @@ function doSaveCollection(json) {
   $('#y').val(metaManager.lastKnownPosition.y)
   $('#z').val(metaManager.lastKnownPosition.z)
   $('#lastKnownScene').val(metaManager.lastKnownScene)
+
+  oobletStats.forEach(ooblet => {
+    if (ooblet.name.length < 1) return
+    if (!~[1, 6, 7].indexOf(ooblet.status)) return
+
+    let oobletDOM = $(`
+    <tr class="ooblet" species="${ooblet.name}" nickName="${ooblet.nickName}" xp="${ooblet.XP}" level="${ooblet.level}" status="${ooblet.status}" rarity="${ooblet.rarity}">
+      <td class="species"><img src="./img/Sprite/${ooblet.name.split('_')[0]}_common.png" /> ${ooblet.name.split('_')[0]}</td>
+      <td class="nickName">
+        <input type="text" style="width: 100% !important;">
+      </td>
+      <td class="level">
+        <input type="text" style="width: 100% !important;">
+      </td>
+      <td class="rarity">
+        <select class="oobletRarity" style="width: 100% !important;">
+          <option value="0">Common</option>
+          <option value="1">Uncommon</option>
+          <option value="2">Gleamy</option>
+        </select>
+      </td>
+    </tr>`)
+
+    oobletDOM.find('.nickName input').val(ooblet.nickName)
+    oobletDOM.find('.level input').val(ooblet.level)
+    oobletDOM.find(`.oobletRarity [value="${ooblet.rarity}"]`)[0].selected = true
+    oobletDOM.end()
+
+    $('.ooblets table tbody').append(oobletDOM)
+  })
 
   // Switch it up!
   containerSwitch('.step4')
@@ -112,6 +144,7 @@ function handleDone() {
     const gameManager = window.saveFile.listOfGameManager[1]
     const invManager = window.saveFile.listOfInventoryManager.filter(inv => inv.playerName === window.playerName)[0]
     const metaManager = window.saveFile.listOfMetaManager[1]
+    const oobletStats = window.saveFile.listOfOobletStats
   
     let empty = $('.saveEditor input').filter(function () {
       return $(this).val().length < 1;
@@ -122,18 +155,37 @@ function handleDone() {
     invManager.sparklePoints.amt = $('#wishies').val()
     invManager.playerLevel = $('#level').val()
     invManager.wateringCanLevel = $('#wateringCanLevel').val()
-    invManager.playerName = $('#name').val()
   
     gameManager.todayDayTag = $('#weather').val()
     gameManager.timeOfDayInTicks = $('#timeOfDayInTicks').val()
   
     metaManager.lastKnownPosition.x = +$('#x').val()
-    metaManager.lastKnownPosition.x = +$('#y').val()
-    metaManager.lastKnownPosition.x = +$('#z').val()
+    metaManager.lastKnownPosition.y = +$('#y').val()
+    metaManager.lastKnownPosition.z = +$('#z').val()
     metaManager.lastKnownScene = $('#lastKnownScene').val()
   
+    // Ooblets
+    $('.ooblet').each(function () {
+      let thisLevel = $(this).find('.level input').val()
+      let thisRarity = $(this).find('.rarity select').val()
+      let thisNickName = $(this).find('.nickName input').val()
+
+      let filteredOoblet = oobletStats.filter(ooblet => ooblet.nickName == $(this).attr('nickName') && ooblet.level == $(this).attr('level') && ooblet.XP == $(this).attr('xp') && ooblet.status == $(this).attr('status') && ooblet.rarity == $(this).attr('rarity') && ooblet.name == $(this).attr('species'))[0]
+
+      if (filteredOoblet.level == thisLevel && filteredOoblet.rarity == thisRarity && filteredOoblet.nickName == thisNickName) return
+
+      // Has been changed, save this
+      filteredOoblet.level = thisLevel
+      filteredOoblet.rarity = thisRarity
+      filteredOoblet.nickName = thisNickName
+
+      let index = oobletStats.findIndex(ooblet => ooblet.nickName == $(this).attr('nickName') && ooblet.level == $(this).attr('level') && ooblet.XP == $(this).attr('xp') && ooblet.status == $(this).attr('status') && ooblet.rarity == $(this).attr('rarity') && ooblet.name == $(this).attr('species'))
+      window.saveFile.listOfOobletStats[index] = filteredOoblet
+    })
+
     containerSwitch('.step5')
   } catch (err) {
+    console.log(err)
     alert('Error with inputs provided!')
   }
 }
